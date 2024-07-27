747459ad8abeb9b2864501a5672ad7ae5ac321cb
dixitsanskar <sanskar.sanjay@gmail.com>
2024-07-27 13:12:59 +0530

    Error fixed

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[ShortLinkController::class, 'index']);
 
-// Route::get('generate-shorten-link', [ShortLinkController::class,'index']);
+Route::get('generate-shorten-link', [ShortLinkController::class,'index']);
 Route::post('generate-shorten-link', [ShortLinkController::class,'store'])->name('generate.shorten.link.post');
 Route::get('{code}', [ShortLinkController::class, 'shortenLink'])->name('shorten.link');
 
